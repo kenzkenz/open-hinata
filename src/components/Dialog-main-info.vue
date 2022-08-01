6831b87f86eb4795cdb057f454dec2c99315bf81
kenotiai <kenotiai@gmail.com>
2022-08-01 21:10:17 +0900

    no message

--- src/components/Dialog-main-info.vue
+++ src/components/Dialog-main-info.vue
@@ -2,7 +2,7 @@
     <v-dialog :dialog="S_mainInfoDialog">
         <div :style="contentSize">
           <p>製作者＝kenzkenz</p>
-          <p><a href="https://github.com/kenzkenz/open-hinata" target="_blank" >github</a> です。</p>
+          <p><a href="https://github.com/kenzkenz/open-hinata" target="_blank" >github</a> です</p>
           <p><a href="https://twitter.com/kenzkenz" target="_blank">twitter</a>です。</p>
         </div>
     </v-dialog>
