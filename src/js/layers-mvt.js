6322846d2c5e6b6763560d7e9ba99575e58ccb26
落合謙次 <kenotiai@gmail.com>
2023-09-01 02:17:38 +0900

    no message

--- src/js/layers-mvt.js
+++ src/js/layers-mvt.js
@@ -4421,7 +4421,7 @@ function yubinkuColorStyleFunction() {
     const textStyle = new Style({
       text: new Text({
         font: "12px sans-serif",
-        text: prop.fullcode + '\n' + prop.name,
+        text: prop.fullcode + '\n\n' + prop.name,
         offsetY: 12,
         fill:  new Fill({
           color:"black"
